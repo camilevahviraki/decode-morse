d819caeef9c072d8f3619a72c002867e4b6f2a59
camilevahviraki <camilevahviraki@gmail.com>
2022-07-14 00:20:04 +0200

    decode morse

--- lib/decode/morse.rb
+++ lib/decode/morse.rb
@@ -6,5 +6,75 @@ module Decode
   module Morse
     class Error < StandardError; end
     # Your code goes here...
+      def decode_char(str)
+        case str
+        when ".-"
+          "A"
+        when "-..."
+          "B"
+        when "-.-."
+          "C"  
+        when "-.."
+          "D"
+        when "."
+          "E"  
+        when "..-."
+          "F"  
+        when "--."
+          "G" 
+        when "...."
+          "H" 
+        when ".."
+          "I"  
+        when ".---"
+          "J"
+        when "-.-"
+          "K"
+        when ".-.."
+          "L"
+        when "--"
+          "M"
+        when "-."
+          "N" 
+        when "---"
+          "O" 
+        when ".--."
+          "P"
+        when "--.-"
+          "Q"
+        when ".-."
+          "R"
+        when "..."
+          "S" 
+        when "-"
+          "T"
+        when "..-"
+          "U"
+        when "...-"
+          "V"
+        when ".--"
+          "W"  
+        when "-..-"
+          "X"
+        when "-.--"
+          "Y"
+        when "--.."
+          "Z"
+        else 
+          ""
+        end          
+      end  
+      # decode sring
+      def decode_string(morse_code)
+      morse_code
+      .split("   ")
+      .map {|char| char
+      .split(" ")
+      .map {|word| decode_char(word)}
+      .join()
+      }
+      .join(" ")
+      end
+        
   end
 end
